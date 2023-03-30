82e1fbe46ae1296273f60bd3a6e6d105858bb0d5
Your Name <moswit7@gmail.com>
2023-03-30 21:23:32 +0700

    test

--- resources/views/employeeHome.blade.php
+++ resources/views/employeeHome.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
                     {{ __('You are logged in!') }}
-                    {{ __('You are พนักงาน') }}
+                    {{ __('You are พนักงาdน') }}
                 </div>
             </div>
         </div>
