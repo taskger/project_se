0096eda8b911ff90e9a055f664149328e6f7ba3d
taskger <chayanon.poo@ku.th>
2023-04-05 05:13:47 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home
 Route::resource('admin/manageruser', AdminmanagerController::class)->middleware('roleStaff');        
 Route::get('admin/adminmanager', [AdminmanagerController::class, 'adminmanager'])->name('manageruser.adminmanager')->middleware('role');        
 Route::get('admin/employeemanager', [AdminmanagerController::class, 'employeemanager'])->name('manageruser.employeemanager')->middleware('role');        
-Route::resource('admin/contacts', ContactController::class)->middleware('role');       
+Route::resource('admin/contacts', ContactController::class);       
 Route::get('admin/addfile', [AddfileController::class, 'addfile'])->name('addfile.index')->middleware('role');
 Route::get('file-upload', [FileUploadController::class, 'index'])->name('file-upload.index');
 Route::post('store', [FileUploadController::class, 'store']);
