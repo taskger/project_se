b1ca670dfcab2ff53e090938dd3e5d21f549a55c
taskger <moswit7@gmail.com>
2023-04-05 18:12:54 +0700

    ..

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -17,12 +17,21 @@ class FileUploadController extends Controller
          
         $validatedData = $request->validate([
          'file' => 'required|mimes:png,jpg,pdf|max:4028',
+         'file2' => 'required|mimes:png,jpg,pdf|max:4028',
+         'file3' => 'required|mimes:png,jpg,pdf|max:4028',
+         'file4' => 'required|mimes:png,jpg,pdf|max:4028',
  
         ]);
  
         $name = $request->file('file')->getClientOriginalName();
+        $name = $request->file('file2')->getClientOriginalName();
+        $name = $request->file('file3')->getClientOriginalName();
+        $name = $request->file('file4')->getClientOriginalName();
  
         $path = $request->file('file')->store('public/files');
+        $path = $request->file('file2')->store('public/files');
+        $path = $request->file('file3')->store('public/files');
+        $path = $request->file('file4')->store('public/files');
  
  
         $save = new File;
@@ -30,7 +39,7 @@ class FileUploadController extends Controller
         $save->name = $name;
         $save->path = $path;
  
-        return redirect('file-upload')->with('status', 'File Has been uploaded successfully in laravel 8');
+        return redirect('file-upload')->with('status', 'File Has been uploaded successfully');
  
     }
 }
