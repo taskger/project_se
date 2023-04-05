c4809504e4380ddf63435a89dec858f56ce0deaa
taskger <moswit7@gmail.com>
2023-04-06 04:44:25 +0700

    ..

--- resources/views/admin/Uploadfile/file-upload.blade.php
+++ resources/views/admin/Uploadfile/file-upload.blade.php
@@ -53,6 +53,7 @@
               <div class="col-md-12">
                   <button type="submit" class="btn" id="submit">Submit</button>
                   <a class="btnreturn" href="{{ route('admin.home') }}">ย้อนกลับ</a>
+                  
               </div>
           </div>     
       </form>
