402712eaca237a37fb14eb55faaa75aed330c103
taskger <moswit7@gmail.com>
2023-04-07 21:41:32 +0700

    a

--- resources/views/select/selectmain.blade.php
+++ resources/views/select/selectmain.blade.php
@@ -1,20 +1,25 @@
+@extends('layouts.app')
+
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+@section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/select.css')}}">
-    <title>test</title>
+    <title>home</title>
 </head>
-
 <body>
-<ul>
-  <li><a href="#home">Jiramet@ku.th</a></li>
-  <li><a href="#news">test</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li style="float:right"><a class="active" href="#about">Home</a></li>
-</ul>
-</nav>
-</body>
-</html>
+<div class="row">
+        
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>EVCARE
+                <a href="http://127.0.0.1:8000/home" > home </a>
+                </h2>
+            </div>
+
+            
+        </div>
+</div>
