21e733dec1ce424e25dc546281865da20403b35f
taskger <moswit7@gmail.com>
2023-04-08 03:52:02 +0700

    123

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -11,6 +11,8 @@
 </head>
 <body>
     @foreach ($userdatas as $usr )
+    <a class="home" href="http://127.0.0.1:8000/home">PROFILE</a>
+    {{$usr->profile_image}}
     <p class="private">ข้อมูลส่วนตัว </p>
     <p class="name">ชื่อ-สกุล: {{$usr->firstname}} {{$usr->lastname}}</p>
     <p class="gen">เพศ:{{$usr->prefix}}</p>
